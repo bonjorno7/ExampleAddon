da2651c5ea92d323b09eac98c9d85bbd14e80e43
bonjorno7 <jorijndegraaf@gmail.com>
2020-08-15 21:53:54 +0200

    Bump version to 0.0.2
    All panels are in the same file now, and derive from a base class.
    The bl_category for the base class is used in prefs too.
    This way you only have to edit it in one place.

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ bl_info = {
     'author': 'bonjorno7',
     'description': 'Example addon for Blender',
     'blender': (2, 80, 0),
-    'version': (0, 0, 1),
+    'version': (0, 0, 2),
     'category': '3D View',
     'location': 'View3D',
 }
