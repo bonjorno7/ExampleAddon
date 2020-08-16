09cd498482b27e761b17149c0fdf01dcbeaf11b7
bonjorno7 <jorijndegraaf@gmail.com>
2020-08-16 23:23:28 +0200

    Remove utils.common.description()
    It's no longer needed here because it lives in utils.ops now.

--- addon/utils/common.py
+++ addon/utils/common.py
@@ -10,9 +10,5 @@ def prefs():
     return bpy.context.preferences.addons[module()].preferences
 
 
-def description(*args):
-    return '.\n'.join(args)
-
-
 def sanitize(text: str):
     return ''.join('_' if c in ':*?"<>|' else c for c in text)
