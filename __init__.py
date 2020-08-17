5d610b433e57440a59016066b42842e49bece321
bonjorno7 <jorijndegraaf@gmail.com>
2020-08-17 16:03:35 +0200

    Bump version to 0.0.4
    Added keymaps and a pie menu.

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ bl_info = {
     'author': 'bonjorno7',
     'description': 'Example addon for Blender',
     'blender': (2, 80, 0),
-    'version': (0, 0, 3),
+    'version': (0, 0, 4),
     'category': '3D View',
     'location': 'View3D',
 }
