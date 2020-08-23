2e660111cf33607658245790bb83d7314cf3422f
bonjorno7 <jorijndegraaf@gmail.com>
2020-08-24 01:51:11 +0200

    Add dropshadow to draw_text
    It's optional but I know I'll be using it.
    The settings are hardcoded to look like the text blender has in the top left of the 3D view.

--- addon/utils/draw_2d.py
+++ addon/utils/draw_2d.py
@@ -2,7 +2,7 @@ import bpy
 import blf
 
 
-def draw_text(text, font, size, x, y, horizontal, vertical):
+def draw_text(text, font, size, x, y, horizontal, vertical, shadow):
     '''
     Draw two aligned columns of text.
 
@@ -13,6 +13,7 @@ def draw_text(text, font, size, x, y, horizontal, vertical):
         x, y: Pixel on the active area.
         horizontal: 'LEFT', 'CENTER', or 'RIGHT'.
         vertical: 'BOTTOM', 'CENTER', or 'TOP'.
+        shadow: Whether to add a dropshadow.
     '''
 
     blf.size(font, size, bpy.context.preferences.system.dpi)
@@ -33,6 +34,11 @@ def draw_text(text, font, size, x, y, horizontal, vertical):
     elif vertical == 'CENTER':
         y += height * len(text) * 0.5
 
+    if shadow:
+        blf.enable(font, blf.SHADOW)
+        blf.shadow(font, 5, 0, 0, 0, 1)
+        blf.shadow_offset(font, 1, -1)
+
     for index, line in enumerate(text):
         offset = (index + 2) * height
 
@@ -44,3 +50,6 @@ def draw_text(text, font, size, x, y, horizontal, vertical):
 
         blf.position(font, x + space, y - offset, 0)
         blf.draw(font, right)
+
+    if shadow:
+        blf.disable(font, blf.SHADOW)
