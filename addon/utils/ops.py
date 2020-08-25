29307a3782849c3e59b003317aee53509ed19ad9
bonjorno7 <jorijndegraaf@gmail.com>
2020-08-25 20:56:07 +0200

    Add hide_hud and show_hud utils
    As part of utils.ops.py
    This hides the operator redo panel and the N panel, and restores them when you're done.
    I decided to make hud_info a parameter rather than a global variable, just in case you might have an operator that calls another operator, and both of them use these utils.
    I'm using if statements instead of just setting the variables because I don't want to needlessly call their update functions. The statusbar would open and then quickly close again, and this fixes that.
    I know the function is called show_hud, but it only shows things that were already enabled when you called hide_hud.

--- addon/utils/ops.py
+++ addon/utils/ops.py
@@ -33,3 +33,42 @@ def cursor_warp(event):
 
     if x != event.mouse_x or y != event.mouse_y:
         bpy.context.window.cursor_warp(x, y)
+
+
+def hide_hud():
+    '''
+    Hide the sidebar and redo panel.
+
+    Returns:
+        hud_info: Two booleans, to be used with show_hud.
+    '''
+
+    space_data = bpy.context.space_data
+    sidebar = space_data.show_region_ui
+    redo = space_data.show_region_hud
+
+    if sidebar:
+        space_data.show_region_ui = False
+
+    if redo:
+        space_data.show_region_hud = False
+
+    return sidebar, redo
+
+
+def show_hud(hud_info):
+    '''
+    Show the sidebar and redo panel.
+
+    Args:
+        hud_info: Two booleans, returned by hide_hud.
+    '''
+
+    sidebar, redo = hud_info
+    space_data = bpy.context.space_data
+
+    if sidebar:
+        space_data.show_region_ui = True
+
+    if redo:
+        space_data.show_region_hud = True
