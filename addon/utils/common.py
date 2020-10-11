fd0517575c2afe499ab4daed54a7f5869ab9db92
bonjorno7 <jorijndegraaf@gmail.com>
2020-10-11 20:19:45 +0200

    Add documents and appdata path utils
    These folders can come in handy when your addon needs a place to store files.

--- addon/utils/common.py
+++ addon/utils/common.py
@@ -1,4 +1,7 @@
 import bpy
+import pathlib
+import platform
+import ctypes.wintypes
 from .. import props
 
 
@@ -24,3 +27,21 @@ def sanitize(text):
     '''
 
     return ''.join('_' if c in ':*?"<>|' else c for c in text)
+
+
+def documents():
+    '''Get the documents folder on Windows, or the home folder on other platforms.'''
+    if platform.system() == 'Windows':
+        buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+
+        ctypes.windll.shell32.SHGetFolderPathW(None, 5, None, 1, buf)
+        return pathlib.Path(buf.value)
+
+    else:
+        return pathlib.Path.home()
+
+
+def appdata():
+    '''Get the path to the appdata folder for this version of Blender.'''
+    user = bpy.utils.resource_path('USER')
+    return pathlib.Path(user).resolve()
